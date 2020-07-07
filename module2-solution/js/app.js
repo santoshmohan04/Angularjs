c3afed7a60d82a57df58cc36f9b6f6e8ef5e8234
santoshmohan04 <you@example.com>
2020-07-07 22:45:57 +0530

    "Assignment 2"

---
+++ module2-solution/js/app.js
@@ -0,0 +1,60 @@
+(function() {
+    'use strict';
+    angular
+        .module('ShoppingListCheckOff', [])
+        .controller('ToBuyShoppingController', ToBuyShoppingController)
+        .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+    ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+
+
+    function ToBuyShoppingController(ShoppingListCheckOffService) {
+        var toBuyShopping = this;
+        toBuyShopping.buyItem = function(itemIndex) {
+            ShoppingListCheckOffService.buyItem(itemIndex);
+        }
+        toBuyShopping.items = ShoppingListCheckOffService.getAvailableItems();
+    }
+
+    AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+
+    function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+        var toBuyShopping = this;
+        toBuyShopping.items = ShoppingListCheckOffService.getBoughtItems();
+    }
+
+    function ShoppingListCheckOffService() {
+        var service = this;
+        var toBuyItems = [{
+            name: "cookies",
+            quantity: 10
+        }, {
+            name: "apples",
+            quantity: 14
+        }, {
+            name: "Mangoes",
+            quantity: 20
+        }, {
+            name: "candles",
+            quantity: 50
+        }, {
+            name: "chairs",
+            quantity: 3
+        }];
+
+        var boughtItems = [];
+
+        service.buyItem = function(itemIndex) {
+            boughtItems.push(toBuyItems[itemIndex]);
+            toBuyItems.splice(itemIndex, 1); // removes item from toBuyItems
+        };
+
+        service.getAvailableItems = function() {
+            return toBuyItems;
+        };
+
+        service.getBoughtItems = function() {
+            return boughtItems;
+        };
+    }
+})();
